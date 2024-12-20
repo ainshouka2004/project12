b34182004aba7a80a8fcb65e02d9e5d7c66c5797
Noura Ainshouka <ynshwktn@gmail.com>
2024-12-20 22:02:40 +0000

    edit1

--- test.java
+++ test.java
@@ -3,6 +3,8 @@ public class test {
 
     public static void main(String[] args) {
         System.out.println("Nouran");
+        System.out.println("Ahmed");
+        System.out.println("100/100");
     }
 }
 

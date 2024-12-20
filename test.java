650e71ad144be466dbcaef7c89f0a34ec11af213
ainshouka2004 <ynshwktn@gmail.com>
2024-12-20 23:03:32 +0000

    edit for nouran_branch

--- test.java
+++ test.java
@@ -4,6 +4,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println("Nouran");
         System.out.println("Ahmed");
+        System.out.println("operating system2");
         System.out.println("100/100");
     }
 }
